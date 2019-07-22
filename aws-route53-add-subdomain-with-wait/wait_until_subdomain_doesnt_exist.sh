28839c1ada0ee2ec5acb12e70b23de395ff4ac5e
alexazarh <aazarh@gmail.com>
2019-07-22 15:04:38 +0300

    fixing wait_until_subdomain_doesnt_exist.sh

--- aws-route53-add-subdomain-with-wait/wait_until_subdomain_doesnt_exist.sh
+++ aws-route53-add-subdomain-with-wait/wait_until_subdomain_doesnt_exist.sh
@@ -6,13 +6,19 @@ wait_interval=5
 domain="$1"
 subdomain="$2"
 
+if [[ "$domain" != *\.  ]]
+then
+    # if domain doesnt end with a dot (.) add a dot otherwise aws cli will not find the domain recorcd in route53
+    domain="$domain."
+fi
+
 for (( c=0 ; c<$timeout ; c=c+$wait_interval ))	
 do
     status=$(aws route53 list-resource-record-sets --hosted-zone-id "$3" --query "ResourceRecordSets[?Name == '$subdomain.$domain']|[?Type == 'A']" | jq 'any')
     if [[ "$status" == "true" ]]
     then
         # domain exists, waiting
-        let remaining=$wait_sec-$c
+        let remaining=$timeout-$c
         echo "Domain $subdomain.$domain exists, sleeping for $wait_interval. Remaining timeout is $remaining seconds."
         unset status  # reset the $status var
         
